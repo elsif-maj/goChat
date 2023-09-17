e436c880922e0c9b165b4eb3e72c0d634ea35748
Michael Johnson <elsif.maj@gmail.com>
2023-09-17 15:34:53 -0700

    bugville

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	m "github.com/elsif-maj/goChat/pkg/mongo"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/net/websocket"
+)
+
+var collection *mongo.Collection
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client:", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		rmtAddrBytes := []byte(ws.RemoteAddr().String() + ": ")
+
+		msg = append(rmtAddrBytes, msg...)
+		m.WriteMsg(collection, msg)
+		s.broadcast(msg)
+	}
+}
+
+func (s *Server) broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("write error:", err)
+			}
+		}(ws)
+	}
+}
+
+func CORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		if r.Method == "OPTIONS" {
+			http.Error(w, "No Content", http.StatusNoContent)
+			return
+		}
+
+		next(w, r)
+	}
+}
+
+func handleHome(collection *mongo.Collection) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		results := m.ReadMsgs(collection)
+
+		// Marshal the results slice into JSON
+		jsonData, err := json.Marshal(results)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Set the Content-Type header to indicate JSON
+		w.Header().Set("Content-Type", "application/json")
+
+		// Write the JSON data to the response writer
+		_, err = w.Write(jsonData)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func main() {
+	server := NewServer()
+
+	db, ctx := m.ConnectToMongo()
+	collection = db.Database("goChat").Collection("goChatCapped")
+
+	defer func() {
+		if err := db.Disconnect(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.HandleFunc("/api/msgs", CORS(handleHome(collection)))
+
+	log.Fatal(http.ListenAndServe(":3001", nil))
+}
---
+++ pkg/mongo/mongo.go
@@ -0,0 +1,98 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MsgEntry struct {
+	ID  primitive.ObjectID `bson:"_id"`
+	Msg string             `bson:"msg"`
+}
+
+type WriteMsgEntry struct {
+	Msg string `bson:"msg"`
+}
+
+func ConnectToMongo() (*mongo.Client, context.Context) {
+	// Set up a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Define MongoDB connection options
+	clientOptions := options.Client().ApplyURI("mongodb+srv://elsifmaj:sekret@fso-part3.hjslzld.mongodb.net/?retryWrites=true&w=majority")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ping the MongoDB server to check if the connection is successful
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// You are now connected to MongoDB!
+	fmt.Println("Connected to MongoDB")
+
+	return client, ctx
+}
+
+func WriteMsg(c *mongo.Collection, msg []byte) error {
+	document := WriteMsgEntry{Msg: string(msg)}
+	// Set up a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	fmt.Println("did we get here?")
+	// Insert the document into the collection
+	_, err := c.InsertOne(ctx, document)
+	if err != nil {
+		fmt.Println("here!")
+		return err
+	}
+
+	return nil
+}
+
+func ReadMsgs(c *mongo.Collection) []MsgEntry {
+	fmt.Println("here's the collection: ", c)
+
+	// Set up a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Find all documents (match all)
+	cursor, err := c.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(ctx)
+
+	results := []MsgEntry{}
+
+	// Iterate through the results
+	for cursor.Next(ctx) {
+		var result MsgEntry // You can define a struct that matches your document structure here instead of bson.M
+		if err := cursor.Decode(&result); err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, result)
+	}
+
+	// Check for cursor errors
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return results
+}
